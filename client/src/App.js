63998b530594b2cb6716e79e20f7dd82998fa19e
Liana Penn <lianapenn@gmail.com>
2017-09-21 17:31:19 -0400

    started app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import SearchForm from './components/SearchForm';
 import './App.css';
 
 class App extends Component {
+constructor() {
+  super();
+  this.state = {
+    animals: [],
+    inputBoroughValue: '',
+  }
+  this.handleBoroughSearch = this.handleBoroughSearch.bind(this);
+  this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+}
+
+handleBoroughSearch(event) {
+  this.setState({
+    inputBoroughValue: event.target.value
+  })
+  console.log(event.target.value)
+}
+
+handleSearchSubmit(event) {
+  //event.preventDefault();
+  event.target.content = '';
+
+  axios(`http://localhost:3001/api/animals/${this.state.inputBoroughValue}`)
+  .then((res) => {
+    this.setState(prevState => {
+      console.log(res.data.data)
+      return {
+      animals: res.data.data.animals
+    }
+    })
+  }).catch(err => console.log(err));
+}
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <SearchForm
+      handleBoroughSearch={this.handleBoroughSearch}
+      handleSearchSubmit={this.handleSearchSubmit}
+      />
       </div>
     );
   }
--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -1,24 +1,25 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 
 class SearchForm extends Component {
     render() {
         return (
             <div id='search-form-container'>
             <form
-                classname='search-form'
-                method='GET'
-                action='/api/animals' 
+                className='search-form'
+                onSubmit={this.props.handleSearchSubmit}
             >
-            <label for='borough'> Choose your borough
-            <select name='borough' type='text'>
+            <div className='borough'> Choose your borough</div>
+            <select name='borough' type='text' onChange={this.props.handleBoroughSearch}>
               <option value='Bronx'>Bronx</option>
               <option value='Brooklyn'>Brooklyn</option>
               <option value='Manhattan'>Manhattan</option>
               <option value='Queens'>Queens</option>
               <option value='Staten Island'>Staten Island</option>
+
+
             </select>
-            </label>
-                <button name="send" type="submit" class="submit">SEARCH</button>
+
+                <button name="send" type="submit" className="submit">SEARCH</button>
             </form>
         </div>
         )
