d0dff552f074733d2cfea60be8bf8585b2dda3c8
Dominick Serrant <sig647@gmail.com>
2017-09-22 14:12:20 -0400

    Flow conditionals for the API call instilled

--- server.js
+++ server.js
@@ -34,13 +34,38 @@ app.get('/api/yelp/:locale/', (req, res) => {
   returning the call to the data before the promise makes yelpCall 'promiseable'. Then we call the promise and error catch on the yelpCall. This gets the data and returns it to the browser side.
 */
     .then(shelter => {
-          const results = shelter.jsonBody.businesses[0];
+      const siComp = shelter.jsonBody.businesses[0].location.city;
+      const qbComp = shelter.jsonBody.businesses[6].location.city;
+
+          if (siComp === 'Staten Island') {
+          const results = shelter.jsonBody.businesses;
           console.log(results)
+
           res.json({
             message: 'ok',
             data: results
           })
+        }
+        else if (qbComp === 'Whitestone') {
+          const results = shelter.jsonBody.businesses[6];
+          console.log(results)
+
+          res.json({
+            message: 'ok',
+            data: results
+          })
+        }
+        else {
+          const results = shelter.jsonBody.businesses[1];
+          console.log(results)
+
+          res.json({
+            message: 'ok',
+            data: results
+          })
+        }
         })
+
     .catch(err => {console.log(err);});
 });
 
